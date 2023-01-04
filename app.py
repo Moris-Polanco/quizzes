be3abfb94e342d620d924568d131b0a8d2657f38
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2023-01-04 05:44:51 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import openai
+import streamlit as st
+import os
 
 # Set up the OpenAI API client
-openai.api_key = 
+openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 
 def generate_questions(text):
